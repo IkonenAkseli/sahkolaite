a8c54703182646ff975125b94a4d21ea9198da05
Akseli Ikonen <akseli.ikonen@tuni.fi>
2023-05-30 18:41:42 +0300

    Implemented min avg and max rendering

--- public/index.js
+++ public/index.js
@@ -2,33 +2,76 @@
 
 
 const leftHeader = document.querySelector('.header-left');
+const smallestDiv = document.querySelector('.smallest');
+const smallestH1 = smallestDiv.querySelector('h1');
+const smallestP = smallestDiv.querySelector('p');
+const avgDiv = document.querySelector('.avg');
+const avgH1 = avgDiv.querySelector('h1');
+const avgP = avgDiv.querySelector('p');
+const maxDiv = document.querySelector('.max');
+const maxH1 = maxDiv.querySelector('h1');
+const maxP = maxDiv.querySelector('p');
 
+
+function setColor(element, price){
+  element.classList.remove('red');
+  element.classList.remove('yellow');
+  element.classList.remove('green');
+  if(price > 10){
+    element.classList.add('red');
+  }
+  else if(price > 5){
+    element.classList.add('yellow');
+  }
+  else {
+    element.classList.add('green');
+  }
+}
 
 
 getPrices().then((prices) => {
   const now = new Date();
   const price = getPriceForDate(now, prices['prices']);
-  leftHeader.innerHTML = `<h1>${now.getHours()}:00 ${price} snt/kWh</h1>`;
-  leftHeader.classList.remove('red');
-  leftHeader.classList.remove('yellow');
-  leftHeader.classList.remove('green');
-  if(price > 10){
-    leftHeader.classList.add('red');
-  }
-  else if(price > 5){
-    leftHeader.classList.add('yellow');
-  }
-  else {
-    leftHeader.classList.add('green');
-  }
-    
+  const hoursNow = now.getHours();
+  leftHeader.innerHTML = `<h1>${hoursNow}:00 - ${hoursNow+1}:00 ${price} snt/kWh</h1>`;
+  setColor(leftHeader, price);
+  
+  setSmallest(prices['prices']);
+  setAvg(prices['prices']);
+  setMax(prices['prices']);
 });
 
+
+function setMax(prices){
+  const max = prices.reduce((prev, current) => (prev.price > current.price) ? prev : current);
+  console.log(max);
+  maxH1.innerHTML = `${new Date(max.startDate).getHours()}:00 - ${new Date(max.endDate).getHours()}:00`;
+  maxP.innerHTML = `${max.price} snt/kWh`;
+  setColor(maxDiv, max.price);
+}
+
+
+function setAvg(prices){
+  const avg = prices.reduce((prev, current) => prev + current.price, 0) / prices.length;
+  console.log(avg);
+  avgH1.innerHTML = `${new Date(prices[0].startDate).getHours()}:00 - ${new Date(prices[prices.length-1].endDate).getHours()}:00`;
+  avgP.innerHTML = `${avg} snt/kWh`;
+  setColor(avgDiv, avg);
+}
+
+
+function setSmallest(prices){
+  const smallest = prices.reduce((prev, current) => (prev.price < current.price) ? prev : current);
+  console.log(smallest);
+  smallestH1.innerHTML = `${new Date(smallest.startDate).getHours()}:00 - ${new Date(smallest.endDate).getHours()}:00`;
+  smallestP.innerHTML = `${smallest.price} snt/kWh`;
+  setColor(smallestDiv, smallest.price);
+}
 
 
 async function getPrices(){
   return fetch('http://localhost:3000/prices').then((response) => {
-    return response.json()
+    return response.json();
   });
 }
 
